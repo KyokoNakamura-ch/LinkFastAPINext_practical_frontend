d56e3578e8e457499d569fcc278887544ceca0a0
KyokoNakamura-ch <kyoko.nakamura.2021@globis.ac.jp>
2025-02-10 11:13:26 +0900

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,29 @@
-require('dotenv').config()
+require('dotenv').config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        // Reference a variable that was defined in the .env file and make it available at Build Time
-        NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
-      },
-}
-
-// module.exports = nextConfig
-module.exports = {
-  output: 'export', // 静的エクスポートの設定（デプロイできたけどアプリ見えない対策）
+  env: {
+    // Reference a variable that was defined in the .env file and make it available at Build Time
+    NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  },
+  output: 'standalone', // ✅ "export" ではなく、"standalone" に変更！
+  experimental: {
+    appDir: true,
+  },
 };
+
+module.exports = nextConfig;
+
+// require('dotenv').config()
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     env: {
+//         // Reference a variable that was defined in the .env file and make it available at Build Time
+//         NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+//       },
+// }
+
+// // module.exports = nextConfig
+// module.exports = {
+//   output: 'export', // 静的エクスポートの設定（デプロイできたけどアプリ見えない対策）
+// };
